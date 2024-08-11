1d9b0a82844d9d0d5876eeb9bbd3ad579aa00960
Tarcisio Ferraz <tarcisio.ferraz@bitfinex.com>
2024-08-11 10:27:52 -0300

    fix weight decay

--- train.py
+++ train.py
@@ -280,7 +280,7 @@ def get_lr(it):
 
 # optimize!
 # optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
-optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
+optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device_type=device)
 
 for i in range(max_steps):
     t0 = time.time()
